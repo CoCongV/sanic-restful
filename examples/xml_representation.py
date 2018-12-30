ba1ba03ec740db80eb1e72f0e8c3a67ede2aed2c
CoCong <cong.lv.yx@gmail.com>
2018-12-30 22:56:13 +0800

    update xml example

--- examples/xml_representation.py
+++ examples/xml_representation.py
@@ -1,15 +1,21 @@
 # needs: pip install python-simplexml
 from simplexml import dumps
-from flask import make_response, Flask
+from sanic import Sanic
+from sanic.response import HTTPResponse
 from sanic_restful import Api, Resource
 
-def output_xml(data, code, headers=None):
+
+def output_xml(app, data, code, headers=None):
     """Makes a Flask response with a XML encoded body"""
-    resp = make_response(dumps({'response' :data}), code)
+    resp = HTTPResponse(
+        dumps({
+            'response': data
+        }), status=code, headers=headers)
     resp.headers.extend(headers or {})
     return resp
 
-app = Flask(__name__)
+
+app = Sanic(__name__)
 api = Api(app, default_mediatype='application/xml')
 api.representations['application/xml'] = output_xml
 
@@ -25,11 +31,11 @@ class Hello(Resource):
         >>> get('http://localhost:5000/me', headers={"accept":"application/xml"}).content
         '<?xml version="1.0" ?><response><hello>me</hello></response>'
     """
-    def get(self, entry):
+    async def get(self, request, entry):
         return {'hello': entry}
 
-api.add_resource(Hello, '/<string:entry>')
+
+api.add_resource(Hello, '/<entry:string>')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
