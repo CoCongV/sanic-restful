10e66481d578b2d3de0cb2624b9438fcfca7c15f
CoCong <cong.lv.yx@gmail.com>
2019-01-03 17:55:08 +0800

    update comment format

--- sanic_restful/fields.py
+++ sanic_restful/fields.py
@@ -357,8 +357,7 @@ def _rfc822(dt):
     """Turn a datetime object into a formatted date.
 
     Example::
-
-        fields._rfc822(datetime(2011, 1, 1)) => "Sat, 01 Jan 2011 00:00:00 -0000"
+    fields._rfc822(datetime(2011, 1, 1)) => "Sat, 01 Jan 2011 00:00:00 -0000"
 
     :param dt: The datetime to transform
     :type dt: datetime
